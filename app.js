d646289094b0dd48bfb11ff0119b05b10d90b375
keremduran <keremduran@live.com>
2022-03-22 19:09:12 -0400

    active menu item, features section &

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const menuItems = document.querySelectorAll('.menuItem');
 let previousIndex = 0;
 menuItems.forEach((item, index) => {
   item.addEventListener('click', () => {
+    if (previousIndex !== index) {
+      item.classList.add('active');
+      menuItems[previousIndex].classList.remove('active');
+    }
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
     sliderTitles[index].style.transform = `scale(1.3, 1.2)`;
     sliderTitles[previousIndex].style.transform = `scale(1)`;
